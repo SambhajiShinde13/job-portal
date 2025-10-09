29ef5263f5971c867d61276dffec72c25ea30f19
Sambha13 <sambhashinde01@gmail.com>
2025-10-09 18:22:29 +0530

    AuthAPI changes

--- frontend/src/Services/AuthService.tsx
+++ frontend/src/Services/AuthService.tsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
-const base_url = "https://careerpoint.duckdns.org/auth/"
-const loginUser = async (login:any)=> {
-    return axios.post(`${base_url}login`, login)
-        .then((result:any) => result.data)
-        .catch((error:any) =>{throw error;});
+import axiosInstance from "../Interceptor/AxiosInterceptor";  // USE THIS
+
+const loginUser = async (login: any) => {
+    return axiosInstance.post(`/auth/login`, login)  // CHANGED
+        .then((result: any) => result.data)
+        .catch((error: any) => { throw error; });
 }
 
-export {loginUser};
+export { loginUser };
